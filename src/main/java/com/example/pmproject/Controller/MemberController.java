d8a9fa3c43ce1cdec56b903c71d6722e09a6c090
adubnos <gp39tu8526@gmail.com>
2023-11-24 16:56:20 +0900

    update

--- src/main/java/com/example/pmproject/Controller/MemberController.java
+++ src/main/java/com/example/pmproject/Controller/MemberController.java
@@ -13,37 +13,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.servlet.HandlerMapping;
 
 import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/member")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/info")
+    @GetMapping({"/member/info","/admin/info"})
     public String memberInfo(Model model, Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         MemberDTO member = memberService.listOne(userDetails.getUsername());
 
         model.addAttribute("member", member);
 
-        return "member/info";
+
+        if("/admin/info".equals(RequestContextHolder.currentRequestAttributes().getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE, RequestAttributes.SCOPE_REQUEST))) {
+            return "admin/info";
+        }else {
+            return "member/info";
+        }
     }
 
-    @GetMapping("/update")
+    @GetMapping({"/member/update","/admin/update"})
     public String updateForm(MemberUpdateDTO memberUpdateDTO, Model model, Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         MemberDTO memberDTO=memberService.listOne(userDetails.getUsername());
         model.addAttribute("memberDTO", memberDTO);
 
-        return "member/update";
+        if("/admin/update".equals(RequestContextHolder.currentRequestAttributes().getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE, RequestAttributes.SCOPE_REQUEST))) {
+            return "admin/update";
+        }else {
+            return "member/update";
+        }
     }
 
-    @PostMapping("/update")
-    public String update(@Valid MemberUpdateDTO memberUpdateDTO, MemberDTO memberDTO, BindingResult bindingResult, Model model, Authentication authentication) {
+    @PostMapping("/member/update")
+    public String memberUpdate(@Valid MemberUpdateDTO memberUpdateDTO, MemberDTO memberDTO, BindingResult bindingResult, Model model, Authentication authentication) {
         if(bindingResult.hasErrors()) {
             return "member/update";
         }
@@ -63,13 +74,38 @@ public class MemberController {
         return "redirect:/member/info";
     }
 
-    @GetMapping("/withdrawal")
-    public String withdrawalForm() {
-        return "member/withdrawal";
+    @PostMapping("/admin/update")
+    public String adminUpdate(@Valid MemberUpdateDTO memberUpdateDTO, MemberDTO memberDTO, BindingResult bindingResult, Model model, Authentication authentication) {
+        if(bindingResult.hasErrors()) {
+            return "admin/update";
+        }
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        try {
+            String result = memberService.update(memberUpdateDTO, userDetails.getUsername());
+            if(result==null) {
+                model.addAttribute("wrongPwd", "기존 비밀번호가 맞지 않습니다.");
+                return "admin/update";
+            }
+        } catch (IllegalStateException e) {
+            model.addAttribute("error", e.getMessage());
+            return "admin/update";
+        }
+
+        return "redirect:/admin/info";
     }
 
-    @PostMapping("/withdrawal")
-    public String withdrawal(@RequestParam String password, Model model, Authentication authentication) {
+    @GetMapping({"/member/withdrawal", "/admin/withdrawal"})
+    public String withdrawalForm() {
+        if("/admin/withdrawal".equals(RequestContextHolder.currentRequestAttributes().getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE, RequestAttributes.SCOPE_REQUEST))) {
+            return "admin/withdrawal";
+        }else {
+            return "member/withdrawal";
+        }
+    }
+
+    @PostMapping("/member/withdrawal")
+    public String memberWithdrawal(@RequestParam String password, Model model, Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         boolean result = memberService.withdrawal(userDetails.getUsername(), password);
 
@@ -78,6 +114,19 @@ public class MemberController {
         }else {
             model.addAttribute("wrongPwd", "비밀번호가 맞지 않습니다.");
             return "member/withdrawal";
+        }
+    }
+
+    @PostMapping("/admin/withdrawal")
+    public String adminWithdrawal(@RequestParam String password, Model model, Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        boolean result = memberService.withdrawal(userDetails.getUsername(), password);
+
+        if(result) {
+            return "redirect:/logout";
+        }else {
+            model.addAttribute("wrongPwd", "비밀번호가 맞지 않습니다.");
+            return "admin/withdrawal";
         }
     }
 }
