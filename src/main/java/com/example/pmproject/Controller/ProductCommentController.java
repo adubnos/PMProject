054ee6153034ba1bd424da5809d3cb199d292fab
adubnos <gp39tu8526@gmail.com>
2023-12-13 15:05:08 +0900

    update

--- src/main/java/com/example/pmproject/Controller/ProductCommentController.java
+++ src/main/java/com/example/pmproject/Controller/ProductCommentController.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/product/detail")
+@RequestMapping("/product/comment")
 public class ProductCommentController {
 
     private final ProductCommentService productCommentService;
